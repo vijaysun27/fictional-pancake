d9c6ad61b0eed858e085876ec640a66ca580ee19
vijaysun27 <vijayguru27@gmail.com>
2025-06-05 12:04:48 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,40 @@
+'''Celsius to Farenheit'''
+
+
+
+'''Farenheit to Celsius'''
+
+# f = float(input("Enter Farenheit: "))
+# c = float(f-32)*5/9
+# print(f"The Celsius is {c}")
+
+'''Calculate Simple Interest'''
+
+# p = float(input("Enter Principal: "))
+# n = float(input("Enter Time: "))
+# r = float(input("Enter rate of interest: "))
+# si = float(p*n*r/100)
+#
+# print(f"The Simple Interest is {si}")
+
+'''Perform Basic Math operations'''
+
+# n1 = float(input("Enter the first number: "))
+# n2 = float(input("Enter the second number: "))
+#
+# print(f"Addition: {n1+n2}")
+# print(f"Subtraction: {n1-n2}")
+# print(f"Multiplication: {n1*n2}")
+# print(f"Division: {n1/n2}")
+
+'''Greeting a member'''
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+full_name= first_name+ " " + last_name
+
+print(f"Hello, {full_name}! Welcome to the Python Program")
+
+
+
+
